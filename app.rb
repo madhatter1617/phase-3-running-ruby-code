a5c2fcb7e90a65fb7c0e58ad9d596fb90995945a
Rosa <rosarenee@att.net>
2021-08-10 02:35:36 -0500

    Done!

---
+++ app.rb
@@ -0,0 +1,36 @@
+## app.rb
+# puts "Hello world!" 
+# #adds a line breaks
+
+# print "Hello biss"
+# #just prints text 
+
+
+# # or it can look like below the () are optional 
+#         # # app.rb
+#         # puts("Hello world!")
+
+# puts [1, 2, 3]
+# #Ruby will coerce the data to a string by calling the .to_s method. 
+# #In the case of arrays, this will produce the following output, with 
+# #each element of the array printed to a new line in the console
+
+# # This is the equivalent of calling:
+# # puts [1, 2, 3].inspect
+# p [1, 2, 3]
+
+
+# pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+
+
+# end of first part ^^
+
+puts "Hello World!"
+print "Pass this test, please."
+p [1,2,3]
+
+# it 'outputs the string "Pass this test, please." using the print method' do
+#     expect { load 'app.rb' }.to output(a_string_including("Pass this test, please.")).to_stdout
+#   end
+
+ 
